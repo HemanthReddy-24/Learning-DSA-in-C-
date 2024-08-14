23b2fd7fa78eefdbb3f566f0c5363e6700127a6f
Hemanth Reddy <kalakotahemanthreddy24@gmail.com>
2024-08-14 21:52:43 +0530

    Linked List Started

--- Arrays/2dArray.cpp
+++ Arrays/2dArray.cpp
@@ -1,23 +1,61 @@
 #include<iostream>
 using namespace std;
 
-void print(int arr[][3], int m, int n) {  // Fixed number of columns
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+
 
 int main() {
     int m = 3, n = 3;
-    int arr[3][3] = {  // Must use constant dimensions here
+    int arr[m][n] = {  // Must use constant dimensions here
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
-    print(arr, m, n);  // Pass the array along with dimensions
+    cout<<endl;
+
+    int key=5;
+    bool flag=false;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j] == key){
+                cout<<i<<" "<<j;
+                flag=true;
+                break;
+            }
+        }
+    }
+    if(flag == false){
+        cout<<"Not found";
+    }
+
+    cout<<endl;
+    for(int i=0;i<m;i++){
+            int max = arr[i][0];
+        for(int j=0;j<n;j++){
+            if(arr[i][j] > max){
+                max=arr[i][j];
+            }
+        }
+        cout<<max<<" ";
+    }
+    cout<<endl;
+
+    for (int j = 0; j < n; j++) {
+        int max = arr[0][j];
+
+        for (int i = 1; i < m; i++) {
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+        cout<<max<<" ";
+    }
+    cout<<endl;
     return 0;
 }
---
+++ Linked Lists/LinkedList.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+void print(Node* node) {
+    while (node != nullptr) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+void InsertAtBegining(Node*& head,int d){
+    Node* newNode = new Node();
+    newNode->data=d;
+    newNode->next = head;
+    head = newNode;
+}
+
+void InsertAtEnd(Node*& head,int d){
+    Node* newNode = new Node();
+    newNode->data=d;
+    newNode->next=nullptr;
+
+    if(head == nullptr){
+        head=newNode;
+        return;
+    }
+
+    Node* temp=head;
+    while(temp->next!=nullptr){
+        temp=temp->next;
+    }
+
+    temp->next=newNode;
+}
+
+int main(){
+    Node* head=nullptr;
+
+    InsertAtBegining(head,5);
+    print(head);
+    InsertAtBegining(head,6);
+    print(head);
+    InsertAtBegining(head,8);
+    print(head);
+
+    InsertAtEnd(head,10);
+    print(head);
+    InsertAtEnd(head,25);
+    print(head);
+    InsertAtEnd(head,48);
+    print(head);
+
+
+    return 0;
+}
