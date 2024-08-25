d5ff6e6ab0e4e863770b6a01a14cf3cf0d88ec1f
Hemanth Reddy <kalakotahemanthreddy24@gmail.com>
2024-08-25 16:49:25 +0530

    Completed Some patterns

---
+++ Pattern Printing/MirroredParallologram.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int m=4,n=6;
+    for(int i=1;i<=m;i++){
+        for(int j=m;j>i;j--){
+            cout<<" ";
+        }
+        for(int j=1;j<=n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
